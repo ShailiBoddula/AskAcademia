4b3d0487f8bbacd93e5b2c977f2da6ec8eb7f386
Shaili Boddula <shaili0722@gmail.com>
2026-05-17 20:53:05 +0530

    Fix CORS allow all origins

--- app/app.py
+++ app/app.py
@@ -18,12 +18,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://ask-academia.vercel.app",
-        "http://localhost:5173",
-        "http://localhost:3000",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
